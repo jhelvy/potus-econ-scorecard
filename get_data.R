c20584d776b453c06ea9fd4b3a700ab9ba8ce78b
emse-p4a-gwu <john.helveston@gmail.com>
2025-04-06 05:37:44 -0400

    use presidents_data on github

--- get_data.R
+++ get_data.R
@@ -14,37 +14,46 @@ library(tidyr)
 #-------------------------------------------
 
 # Define presidential data
-presidents_data <- data.frame(
-  president = c(
-    "Eisenhower (1957)", "Kennedy (1961)", "Johnson (1963)", "Nixon (1969)", 
-    "Nixon (1973)", "Ford (1974)", "Carter (1977)", "Reagan (1981)",
-    "Reagan (1985)", "Bush Sr. (1989)", "Clinton (1993)", "Clinton (1997)",
-    "Bush Jr. (2001)", "Bush Jr. (2005)", "Obama (2009)", "Obama (2013)",
-    "Trump (2017)", "Biden (2021)", "Trump (2025)"
-  ),
-  inauguration_date = as.Date(c(
-    "1957-01-20", "1961-01-20", "1963-11-22", "1969-01-20",
-    "1973-01-20", "1974-08-09", "1977-01-20", "1981-01-20",
-    "1985-01-20", "1989-01-20", "1993-01-20", "1997-01-20",
-    "2001-01-20", "2005-01-20", "2009-01-20", "2013-01-20",
-    "2017-01-20", "2021-01-20", "2025-01-20"
-  )),
-  election_date = as.Date(c(
-    "1956-11-06", "1960-11-08", "1960-11-08", "1968-11-05",
-    "1972-11-07", "1972-11-07", "1976-11-02", "1980-11-04",
-    "1984-11-06", "1988-11-08", "1992-11-03", "1996-11-05",
-    "2000-11-07", "2004-11-02", "2008-11-04", "2012-11-06",
-    "2016-11-08", "2020-11-03", "2024-11-05"
-  )),
-  party = c(
-    "Republican", "Democratic", "Democratic", "Republican",
-    "Republican", "Republican", "Democratic", "Republican",
-    "Republican", "Republican", "Democratic", "Democratic",
-    "Republican", "Republican", "Democratic", "Democratic",
-    "Republican", "Democratic", "Republican"
-  ),
+# presidents_data <- data.frame(
+#   president = c(
+#     "Eisenhower (1957)", "Kennedy (1961)", "Johnson (1963)", "Nixon (1969)", 
+#     "Nixon (1973)", "Ford (1974)", "Carter (1977)", "Reagan (1981)",
+#     "Reagan (1985)", "Bush Sr. (1989)", "Clinton (1993)", "Clinton (1997)",
+#     "Bush Jr. (2001)", "Bush Jr. (2005)", "Obama (2009)", "Obama (2013)",
+#     "Trump (2017)", "Biden (2021)", "Trump (2025)"
+#   ),
+#   inauguration_date = as.Date(c(
+#     "1957-01-20", "1961-01-20", "1963-11-22", "1969-01-20",
+#     "1973-01-20", "1974-08-09", "1977-01-20", "1981-01-20",
+#     "1985-01-20", "1989-01-20", "1993-01-20", "1997-01-20",
+#     "2001-01-20", "2005-01-20", "2009-01-20", "2013-01-20",
+#     "2017-01-20", "2021-01-20", "2025-01-20"
+#   )),
+#   election_date = as.Date(c(
+#     "1956-11-06", "1960-11-08", "1960-11-08", "1968-11-05",
+#     "1972-11-07", "1972-11-07", "1976-11-02", "1980-11-04",
+#     "1984-11-06", "1988-11-08", "1992-11-03", "1996-11-05",
+#     "2000-11-07", "2004-11-02", "2008-11-04", "2012-11-06",
+#     "2016-11-08", "2020-11-03", "2024-11-05"
+#   )),
+#   party = c(
+#     "Republican", "Democratic", "Democratic", "Republican",
+#     "Republican", "Republican", "Democratic", "Republican",
+#     "Republican", "Republican", "Democratic", "Democratic",
+#     "Republican", "Republican", "Democratic", "Democratic",
+#     "Republican", "Democratic", "Republican"
+#   ),
+#   stringsAsFactors = FALSE
+# )
+#
+# write.csv(presidents_data, "presidents_data.csv", row.names = FALSE)
+
+presidents_data <- read.csv(
+  "https://raw.githubusercontent.com/jhelvy/presidential-econ-tracker/refs/heads/main/presidents_data.csv", 
   stringsAsFactors = FALSE
 )
+presidents_data$inauguration_date <- as.Date(presidents_data$inauguration_date)
+presidents_data$election_date <- as.Date(presidents_data$election_date)
 
 # Define market indices to fetch
 market_indices <- data.frame(
@@ -113,8 +122,5 @@ combined_data$data_retrieved <- Sys.Date()
 
 # Save the data to CSV
 write.csv(combined_data, "market_data.csv", row.names = FALSE)
-
-# Save the presidents data to CSV as well
-write.csv(presidents_data, "presidents_data.csv", row.names = FALSE)
 
 message("Data collection complete. Files saved as market_data.csv and presidents_data.csv")
